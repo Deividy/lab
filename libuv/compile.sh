40376cd2c6270fd042766bce04f0fe5a4433f0b9
Deividy Metheler <deividyz@gmail.com>
2014-02-23 17:21:50 -0300

    minor tweak compile.sh

--- libuv/compile.sh
+++ libuv/compile.sh
@@ -1,2 +1,2 @@
-gcc -g -Wall -I../../lib/libuv/include -framework CoreServices -o $1 $2 ../../lib/libuv/libuv.a -v
+gcc -g -Wall -I../../lib/libuv/include -framework CoreServices -o $1 $1.c ../../lib/libuv/libuv.a -v
 rm -rf $1.dSYM
