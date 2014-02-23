6119322ee576271fb4573a0942c91ba10a69f65b
Deividy Metheler <deividyz@gmail.com>
2014-02-23 17:26:05 -0300

    add bin folder

--- libuv/compile.sh
+++ libuv/compile.sh
@@ -1,2 +1,2 @@
-gcc -g -Wall -I../../lib/libuv/include -framework CoreServices -o $1 $1.c ../../lib/libuv/libuv.a -v
+gcc -g -Wall -I../../lib/libuv/include -framework CoreServices -o bin/$1 $1.c ../../lib/libuv/libuv.a -v
 rm -rf $1.dSYM
