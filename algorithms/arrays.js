cb27700642bf95c5dbfd532ea02974d13bd09bc5
Deividy Metheler <deividyz@gmail.com>
2014-07-27 15:41:26 -0300

    add tests for array

---
+++ algorithms/arrays.js
@@ -0,0 +1,33 @@
+assert = require('assert');
+
+Object.defineProperty(Array.prototype, 'minimum', {
+    get: function () {
+        var minimum = null;
+
+        this.forEach(function (item) {
+            if (minimum === null || item < minimum) {
+                minimum = item;
+            }
+        });
+
+        return minimum;
+    }
+});
+
+
+Object.defineProperty(Array.prototype, 'maximum', {
+    get: function () {
+        var maximum = null;
+
+        this.forEach(function (item) {
+            if (maximum === null || item > maximum) {
+                maximum = item;
+            }
+        });
+
+        return maximum;
+    }
+});
+
+assert.equal([ 5, 10, 200, 1, 3, 0 ].minimum, 0);
+assert.equal([ 5, 10, 200, 1, 3, 0 ].maximum, 200);
