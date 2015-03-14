f5095ee20574567dd20bc674a4c2737624648af5
Deividy Metheler <deividyz@gmail.com>
2015-03-14 15:01:08 -0300

    funs with sizeof

---
+++ cc/funs/info.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    cout << "Size of char: " << sizeof(char) << "\n";
+    cout << "Size of bool: " << sizeof(bool) << "\n";
+    cout << "Size of short: " << sizeof(short) << "\n";
+    cout << "Size of int: " << sizeof(int) << "\n";
+    cout << "Size of std::string: " << sizeof(std::string) << "\n";
+};
