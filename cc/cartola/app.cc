9eb782f013787a6a455cd551662b5ea72366dc9a
Deividy Metheler <deividyz@gmail.com>
2016-01-24 15:32:26 -0200

    cartola design tests

---
+++ cc/cartola/app.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+class Jogador {
+    // name
+}
+
+class Time {
+    // jogadores
+}
+
+class Jogo {
+    // home_time
+    // visitant_time
+    // score
+    // winner_time
+}
+
+class Rodada {
+    // times
+    // jogos
+
+}
+
+class Campeonato {
+    // rodadas
+    //
+    // pontos_por_time
+    // classificacao
+}
+
+class Cartola {
+    // campeonato brasileiro
+    //
+    // total_pontos_por_jogador
+    // total_pontos_por_jogador_home
+    // total_pontos_por_jogador_away
+    //
+    // total_pontos_por_jogador_vs_time { jogador: { time, ponto } }
+    //
+    // pontos_por_rodada { 1: [ { time_home, time_away, pontos: { jogador: ponto, jogador: ponto} }] }
+}
+
+class Calculator {
+    // Cartola
+    //
+    // bestGoleiro (home_time, away_time, qty = 1)
+    // bestZagueiros (home_time, away_time, qty = 2)
+    // bestLaterals (home_time, away_time, qty = 2)
+    //
+    // bestTimes
+}
+
+int main() {
+
+}
