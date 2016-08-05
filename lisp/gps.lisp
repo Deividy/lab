7b1e102090e48401fc9a26d0e66f5c97e930d678
Deividy Metheler <deividyz@gmail.com>
2016-08-05 15:29:14 -0300

    add more elaborate version of gps lisp

--- lisp/gps.lisp
+++ lisp/gps.lisp
@@ -56,3 +56,71 @@
              :add-list '(have-money))))
 
 (print (gps '(have-will) '(stoned) *smoking-ops*))
+
+
+; === new version!
+
+(defun gps (state goals *ops*)
+  "GPS: General Problem Solver"
+  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
+
+(defun executing-p (action)
+  "is action of the form: (executing ...)?"
+  (starts-with action 'executing))
+
+(defun starts-with (list val)
+  "is this a list whose first element is the val?"
+  (and (consp list) (eql (first list) val)))
+
+(defun convert-op (op)
+  "convert an operation action to the (executing op) convetion"
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op)) (op-add-list op)))
+  op)
+
+(defun op (action &key preconds add-list del-list)
+  "helper to make a new operator with the (executing op) convetion for action"
+  (convert-op
+    (make-op :action action :preconds preconds
+             :add-list add-list :del-list del-list)))
+
+(defun achieve-all (state goals goal-stack)
+  "achieve each goal, and make sure they still hold at the end"
+  (let ((current-state state))
+    (if (and (every #'(lambda (g)
+                        (setf current-state
+                              (achieve current-state g goal-stack)))
+                    goals)
+             (subsetp goals current-state :test #'equal))
+      current-state)))
+
+(defun achieve (state goal goal-stack)
+  "a goal is achieved when it arrives to its destine :)"
+  (cond ((member-equal goal state) state)
+        ((member-equal goal goal-stack) nil)
+        (t (some #'(lambda (op) (apply-op state goal op goal-stack))
+                 (find-all goal *ops* :test #'appropriate-p)))))
+
+(defun appropriate-p (goal op)
+  "an op is appropriate to a goal if it is in its add-list"
+  (member-equal goal (op-add-list op)))
+
+(defun member-equal (item list)
+  (member item list :test #'equal))
+
+
+(defun apply-op (state goal op goal-stack)
+  "return a new, transformed state if op is applicable"
+  (let ((state2 (achieve-all state (op-preconds op)
+                             (cons goal goal-stack))))
+    (unless (null state2)
+      ;; return an updated state
+      (append (remove-if #'(lambda (s)
+                             (member-equal s (op-del-list op)))
+                         state2)
+              (op-add-list op)))))
+
+
+(mapc #'convert-op *smoking-ops*)
+
+(print (gps '(have-will) '(stoned) *smoking-ops*))
