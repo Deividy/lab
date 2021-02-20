d7088dfea768f691a4be3587cc0b57b0befa1d70
Deividy Metheler Zachetti <deividyz@gmail.com>
2021-02-20 15:10:15 -0300

    add chop elixir test

--- elixir/mm/exercises.exs
+++ elixir/mm/exercises.exs
@@ -7,3 +7,18 @@ defmodule GcdModule do
   def gcd(x, 0), do: x
   def gcd(x, y), do: gcd(y, rem(x, y))
 end
+
+defmodule Chop do
+  def guess(actual, actual.._min), do: "You found it #{actual}"
+  def guess(actual, _min..actual), do: "You found it #{actual}"
+
+  def guess(actual, _min..max) when max < actual, do: guess(actual, max..max * 2)
+  def guess(actual, min.._max) when min > actual, do: guess(actual, min - 1..min)
+
+  def guess(actual, min..max) do
+    my_guess = div(min + max, 2)
+    IO.puts("Is it #{my_guess}?")
+
+    guess(actual, min..my_guess)
+  end
+end
